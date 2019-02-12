b46992b24d3edfd7651479876453e46d0a7a554a
Yahya Daud <ydaud@ucsd.edu>
2019-02-12 00:46:41 -0800

    Day Class and StepHistory Class

---
+++ app/src/main/java/com/example/team17_personalbest/Day.java
@@ -0,0 +1,80 @@
+package com.example.team17_personalbest;
+
+
+import java.util.Calendar;
+
+public class Day {
+
+    private int day;
+    private int plannedSteps;
+    private int normalSteps;
+
+
+    public Day(){
+        Calendar calendar = Calendar.getInstance();
+        day = calendar.get(Calendar.DAY_OF_WEEK);
+        plannedSteps = 0;
+        normalSteps = 0;
+    }
+
+    public Day(int testDay){
+        day = testDay;
+        plannedSteps = 0;
+        normalSteps = 0;
+    }
+
+    public String getDay(){
+        String currDay;
+        currDay = "ERROR!";
+        switch(day) {
+            case Calendar.SUNDAY:
+                currDay = "Sunday";
+                break;
+            case Calendar.MONDAY:
+                currDay = "Monday";
+                break;
+            case Calendar.TUESDAY:
+                currDay = "Tuesday";
+                break;
+            case Calendar.WEDNESDAY:
+                currDay = "Wednesday";
+                break;
+            case Calendar.THURSDAY:
+                currDay = "Thursday";
+                break;
+            case Calendar.FRIDAY:
+                currDay = "Friday";
+                break;
+            case Calendar.SATURDAY:
+                currDay = "Saturday";
+                break;
+        }
+
+        return currDay;
+    }
+
+    public int getNormalSteps() {
+        return normalSteps;
+    }
+
+    public int getPlannedSteps() {
+        return plannedSteps;
+    }
+
+    public void setNormalSteps(int normalSteps) {
+        this.normalSteps = normalSteps;
+    }
+
+    public void setPlannedSteps(int plannedSteps) {
+        this.plannedSteps = plannedSteps;
+    }
+
+    public void addNormalSteps(int normalSteps) {
+        this.normalSteps += normalSteps;
+    }
+
+    public void addPlannedSteps(int plannedSteps) {
+        this.plannedSteps += plannedSteps;
+    }
+
+}
