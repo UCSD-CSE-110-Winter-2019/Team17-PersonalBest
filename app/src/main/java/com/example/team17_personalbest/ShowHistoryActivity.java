cf8c30af4955dea6385242e99eb5ba0e30f5e6ba
Yahya Daud <ydaud@ucsd.edu>
2019-02-24 22:55:32 -0800

    fixed same issue in ShowHistory

--- app/src/main/java/com/example/team17_personalbest/ShowHistoryActivity.java
+++ app/src/main/java/com/example/team17_personalbest/ShowHistoryActivity.java
@@ -21,6 +21,7 @@ import com.github.mikephil.charting.formatter.StackedValueFormatter;
 import com.google.gson.Gson;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ShowHistoryActivity extends AppCompatActivity {
 
@@ -103,13 +104,27 @@ public class ShowHistoryActivity extends AppCompatActivity {
      * Loads the user settings and history from sharedPreferences
      */
     public void loadUser() {
+
         SharedPreferences sharedPreferences = getSharedPreferences("user", MODE_PRIVATE);
         Gson gson = new Gson();
-        String userjson = sharedPreferences.getString("user", "");
-        if (userjson.equals("")){
+        int height = sharedPreferences.getInt("height", 0);
+        if (height == 0){
             user = null;
         } else {
-            user = new User(gson.fromJson(userjson, User.class));
+
+            user= new User(height, Calendar.getInstance());
+            StepHistory stepHistory = gson.fromJson(sharedPreferences.getString("stepHist", ""), StepHistory.class);
+            PlannedWalk plannedWalk = gson.fromJson(sharedPreferences.getString("plannedWalk", ""), PlannedWalk.class);
+            Day day = gson.fromJson(sharedPreferences.getString("day", ""), Day.class);
+            user.setGoal(sharedPreferences.getInt("goal", 0));
+            user.setTotalDailySteps(sharedPreferences.getInt("daily_steps", 0));
+            user.setHasBeenEncouragedToday(sharedPreferences.getBoolean("encouraged", false));
+            user.setHasBeenCongratulatedToday(sharedPreferences.getBoolean("congratulated",false));
+            user.setStepHistory(stepHistory);
+            user.setCurrentWalk(plannedWalk);
+            user.setCurrentDayStats(day);
+
+
         }
     }
 }
