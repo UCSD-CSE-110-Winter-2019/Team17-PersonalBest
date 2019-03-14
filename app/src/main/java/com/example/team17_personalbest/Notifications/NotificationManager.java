8eb9d0d967be01fe82434aca8802901c3c191178
Sai Chandhrasekhar <skchandhrasekhar@gmail.com>
2019-03-13 17:15:54 -0700

    created Notification Manager

---
+++ app/src/main/java/com/example/team17_personalbest/Notifications/NotificationManager.java
@@ -0,0 +1,8 @@
+package com.example.team17_personalbest.Notifications
+
+import com.example.team17_personalbest.User;
+import com.example.team17_personalbest.Firestore.FirebaseAdapter
+
+public class NotificationManager {
+	
+}
